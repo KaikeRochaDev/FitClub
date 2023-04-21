75253bfa2e3df1e560eee7ad7b91bcc945965f35
Kaike Rocha <dev.kaikerocha@gmail.com>
2023-04-21 14:10:29 -0300

    Left part of the header ready and styled

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,6 +8,50 @@ const Hero = () => {
     <div className="hero">
         <div className="left-h">
             <Header/>
+
+            <div className="the-best-ad">
+                <div></div>
+                <span>the best fitness club in the town</span>
+            </div>
+
+            <div className="hero-text">
+                <div>
+                    <span className='stroke-text'>Shape</span> 
+                    <span> Your</span>
+                </div>
+                <div>
+                    <span>Ideal body</span> 
+                </div>
+
+                <div>
+                    <span>
+                        In here we will help you to shape and build your ideal body and live up your life to fullest
+                    </span>
+                </div>
+            </div>
+
+            <div className="figures">
+                <div>
+                    <span>+140</span>
+                    <span>expert coachs</span>
+
+                </div>
+
+                <div>
+                    <span>+978</span>
+                    <span>members joined</span>
+                </div>
+
+                <div>
+                    <span>+50</span>
+                    <span>fitness programs</span>
+                </div>
+            </div>
+
+            <div className="hero-buttons">
+                <button className='btn'>Get Started</button>
+                <button className='btn'>Learn More</button>
+            </div>
         </div>
 
         <div className="right-h">
