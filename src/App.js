1348335fe0740314520c8cbc5c9861fc4ead2e0f
Kaike Rocha <dev.kaikerocha@gmail.com>
2023-04-22 16:00:51 -0300

    Footer completed and styled

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
 
 import Hero from './components/Hero/Hero';
 import Join from './components/Join/Join';
@@ -21,6 +22,8 @@ function App() {
       <Testimonials />
 
       <Join />
+
+      <Footer/>
     </div>
   );
 }
